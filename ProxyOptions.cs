33036f58097bacc3ccb8f6b49e4a4ccebcbec481
Frode Hus <frode.hus@outlook.com>
2022-02-26 12:29:30 +0100

    Feat/commandargs (#2)
    * added better command line parsing
    
    * stuff

---
+++ ProxyOptions.cs
@@ -0,0 +1,11 @@
+using CommandLine;
+
+namespace SwaggerProxy;
+
+public class ProxyOptions
+{
+        [Option('f', "config-file", Required = true, HelpText = "Configuration file to use")]
+        public string ConfigFile { get; set; } = "";
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+}
