096b0f3d7fa6f2ceac9716c526d55abfab1898d9
frodehus <frode.hus@outlook.com>
2022-02-26 22:59:15 +0100

    added basic metrics using prometheus at /metrics

---
+++ Infrastructure/ProxyMetrics.cs
@@ -0,0 +1,16 @@
+using Prometheus;
+
+namespace TinyProxy.Infrastructure;
+
+public static class ProxyMetrics
+{
+    private static readonly Gauge TotalRequests = Metrics.CreateGauge("total_requests", $"Total requests for all endpoints proxied", new GaugeConfiguration
+    {
+        LabelNames = new []{"remote", "route"}
+    });
+    
+    public static void IncomingRequest(ProxyRoute route)
+    {
+        TotalRequests.WithLabels(route.RemoteServer, route.RelativePath).Inc();
+    }
+}
