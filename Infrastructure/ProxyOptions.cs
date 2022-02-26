1e0be9cd5d1a7ec3272bc4d6b5c5b36878a4fb88
frodehus <frode.hus@outlook.com>
2022-02-26 13:15:34 +0100

    added functionality for creating empty config file

--- Infrastructure/ProxyOptions.cs
+++ Infrastructure/ProxyOptions.cs
@@ -1,11 +1,12 @@
 using CommandLine;
 
 namespace TinyProxy.Infrastructure;
-
+[Verb("start", HelpText = "Start proxy")]
 public class ProxyOptions
 {
         [Option('f', "config-file", Required = true, HelpText = "Configuration file to use")]
         public string? ConfigFile { get; set; }
+
         [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
         public bool Verbose { get; set; }
 }
