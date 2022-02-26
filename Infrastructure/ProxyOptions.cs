e3a8041eb55530201f5358d3f9efe0323a00f09b
frodehus <frode.hus@outlook.com>
2022-02-26 12:53:18 +0100

    added new project structure

--- Infrastructure/ProxyOptions.cs
+++ Infrastructure/ProxyOptions.cs
@@ -1,11 +1,11 @@
 using CommandLine;
 
-namespace TinyProxy;
+namespace TinyProxy.Infrastructure;
 
 public class ProxyOptions
 {
         [Option('f', "config-file", Required = true, HelpText = "Configuration file to use")]
-        public string ConfigFile { get; set; } = "";
+        public string? ConfigFile { get; set; }
         [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
         public bool Verbose { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using CommandLine;
-using TinyProxy;
+using TinyProxy.Infrastructure;
+using TinyProxy.Server;
 
 var options = new ProxyOptions();
 
@@ -7,34 +8,8 @@ Parser.Default.ParseArguments<ProxyOptions>(args)
     .WithParsed<ProxyOptions>(o =>
     {
         options = o;
-        if(string.IsNullOrEmpty(options.ConfigFile)) Environment.Exit(0);
-        if (File.Exists(options.ConfigFile)) return;
-        Console.WriteLine($"ERROR: {options.ConfigFile} not found");
-        Environment.Exit(1);
     });
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpForwarder();
-builder.Services.AddHttpClient();
-builder.Services.AddSingleton<RouteConfigurator>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    var routeConfigurator = endpoints.ServiceProvider.GetService<RouteConfigurator>();
-    if (routeConfigurator == null)
-    {
-        throw new ArgumentNullException(nameof(routeConfigurator));
-    }
-
-    routeConfigurator.MapEndpoints(endpoints, options.ConfigFile);
-});
-app.Run();
+var proxy = new Proxy();
+proxy.Configure(args, options);
+proxy.Start();
