daf5564bc0d38e4689effca470a2cd00b3380e1d
frodehus <frode.hus@outlook.com>
2022-03-25 11:13:25 +0100

    added roboto font

--- tinyproxy_dashboard/pages/_app.tsx
+++ tinyproxy_dashboard/pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
